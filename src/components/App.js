e13f46f528e30fa5e80aea4ca227b1823cc19490
Laura Portillo <lauraportillo.ark@gmail.com>
2021-06-06 12:17:55 +0200

    start create components

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import '../stylesheets/App.scss';
+
+const App = () => {
+  //estados
+  const [name, setName] = useState('');
+
+  return <div className="App"></div>;
+};
+
+export default App;
---
+++ src/components/Compo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../stylesheets/Compo.scss';
+
+const Compo = (props) => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+export default Compo;
