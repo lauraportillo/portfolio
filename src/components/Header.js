3f268d11c880405a4e38b5b6215fcbd0293c94ca
Laura Portillo <lauraportillo.ark@gmail.com>
2021-06-10 14:36:48 +0200

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,21 +4,21 @@ import '../stylesheets/Header.scss';
 
 const Header = (props) => {
   return (
-    <>
+    <header className="containerHeader">
       <h1>laura portillo rodríguez</h1>
       <h3>From buildings to code, always structuring</h3>
       <nav>
-        <ul>
-          <li>
+        <ul className="menu">
+          <li className="menu__item">
             <Link to="/">Home</Link>
           </li>
-          <li>|</li>
-          <li>
+          <li className="menu__item">|</li>
+          <li className="menu__item">
             <Link to="/about">About me</Link>
           </li>
         </ul>
       </nav>
-    </>
+    </header>
   );
 };
 export default Header;
