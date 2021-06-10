053de2d252f84c3e01925e09a74f244441050361
Laura Portillo <lauraportillo.ark@gmail.com>
2021-06-10 11:29:03 +0200

    create house component

--- src/components/House.js
+++ src/components/House.js
@@ -2,13 +2,17 @@ import React from 'react';
 import '../stylesheets/House.scss';
 
 const House = (props) => {
+  const houseTech = props.house.technologies.map((technology, index) => {
+    return <li key={index}>{technology}</li>;
+  });
+
   return (
     <article>
       <div>
         <div>
           <img src={props.house.image} alt={props.house.name} />
         </div>
-        <ul></ul>
+        <ul> {houseTech}</ul>
       </div>
       <h2>{props.house.name}</h2>
     </article>
