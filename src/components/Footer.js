ffd4b930d8d74394c4470206de330113f6d20333
Laura Portillo <lauraportillo.ark@gmail.com>
2021-06-11 13:39:52 +0200

    start organize footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,34 +4,27 @@ import '../stylesheets/Footer.scss';
 const Footer = () => {
   return (
     <footer className="containerFooter">
-      <div>
-        <a className="linkIcon" href="https://twitter.com/LauraPo02860847" target="_blank">
+      <div className="containerFooter__icons">
+        <a className="containerFooter__icons--link" href="https://twitter.com/LauraPo02860847" target="_blank">
           <i className="fa fa-twitter" alt="twitter icon"></i>
         </a>
-      </div>
-
-      <div>
         <a
-          className="linkIcon"
+          className="containerFooter__icons--link"
           href="https://www.linkedin.com/in/laura-portillo-rodr%C3%ADguez-21965a86/"
           target="_blank"
         >
           <i className="fa fa-linkedin" alt="linkedin icon"></i>
         </a>
-      </div>
 
-      <div>
-        <a className="linkIcon" href="https://github.com/lauraportillo" target="_blank">
+        <a className="containerFooter__icons--link" href="https://github.com/lauraportillo" target="_blank">
           <i className="fa fa-github-alt" alt="github icon"></i>
         </a>
-      </div>
 
-      <div>
-        <a className="linkIcon" href="mailto:lauraportillo.ark@gmail.com" target="_blank">
+        <a className="containerFooter__icons--link" href="mailto:lauraportillo.ark@gmail.com" target="_blank">
           <i className="fa fa-envelope" alt="envelope icon"></i>
         </a>
       </div>
-      <div>
+      <div className="containerFooter__copy">
         <small>By Laura Portillo Rodríguez</small>
         <small>©2021</small>
       </div>
