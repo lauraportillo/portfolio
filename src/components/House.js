0d515391bd3ff10abb474feef3c0a8f49c41d0cc
Laura Portillo <lauraportillo.ark@gmail.com>
2021-06-14 13:08:54 +0200

    try a hover in house shape

--- src/components/House.js
+++ src/components/House.js
@@ -7,8 +7,8 @@ const House = (props) => {
   });
 
   return (
-    <article className="house">
-      <a href={props.house.url} target="_blank">
+    <article>
+      <a href={props.house.url} target="_blank" className="house">
         <div className="house__shape">
           <div className="house__shape--image">
             <img className="photo" src={props.house.image} alt={props.house.name} />
