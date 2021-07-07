e5cd178aa609c30e5403380a9d577e2655be9053
Laura Portillo <lauraportillo.ark@gmail.com>
2021-07-07 23:26:26 +0200

    hide technologies symbols

--- src/components/House.js
+++ src/components/House.js
@@ -34,9 +34,9 @@ const House = (props) => {
           <div className="house__shape--image">
             <img className="photo" src={props.house.image} alt={props.house.name} />
           </div>
-          <ul className="house__shape--tech"> {houseTech}</ul>
         </div>
         <h2 className="house__title">{props.house.name}</h2>
+        {/* <ul className="house__shape--tech"> {houseTech}</ul> */}
       </a>
     </article>
   );
