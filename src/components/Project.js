2dc18e46ec752326f825760ce23208b8940a55af
Laura Portillo <lauraportillo.ark@gmail.com>
2021-07-08 13:58:22 +0200

    adjust margins

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,26 +3,9 @@ import '../stylesheets/Project.scss';
 
 const Project = (props) => {
   const projectTech = props.project.technologies.map((technology, index) => {
-    let techColor = '';
-    if (technology.includes('react')) {
-      techColor = 'react';
-    } else if (technology.includes('html5')) {
-      techColor = 'html5';
-    } else if (technology.includes('css3')) {
-      techColor = 'css3';
-    } else if (technology.includes('sass')) {
-      techColor = 'sass';
-    } else if (technology.includes('javascript')) {
-      techColor = 'javascript';
-    } else if (technology.includes('git')) {
-      techColor = 'git';
-    } else {
-      techColor = 'gitHub';
-    }
-
     return (
-      <li className={`icon ${techColor}`} key={index}>
-        <img className="icon__image" src={technology} alt="icons" />
+      <li key={index}>
+        <h4 className="icon">{technology}</h4>
       </li>
     );
   });
@@ -36,8 +19,8 @@ const Project = (props) => {
           </div>
         </div>
         <h2 className="project__title">{props.project.name}</h2>
-        <h3 className="project__subtitle">technologies:</h3>
-        {/* <ul className="project__shape--tech"> {projectTech}</ul> */}
+        <h3 className="project__subtitle">technologies: </h3>
+        <ul className="project__shape--tech"> {projectTech}</ul>
       </a>
     </article>
   );
