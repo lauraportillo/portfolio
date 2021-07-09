9b4de90137b5d0724aea5cfcb37399c5838721d2
Laura Portillo <lauraportillo.ark@gmail.com>
2021-07-09 08:57:21 +0200

    add section 1

--- src/components/About.js
+++ src/components/About.js
@@ -3,13 +3,13 @@ import '../stylesheets/About.scss';
 
 const About = (props) => {
   return (
-    <article>
-      <section>
-        <div>
-          <h2>Front-end Developer & Architect</h2>
-          <h3>very creative, professional, easy going and organized.</h3>
+    <article className="about">
+      <section className="section1">
+        <div className="section1__left">
+          <h2 className="section1__left--title">Front-end Developer & Architect</h2>
+          <h3 className="section1__left--subtitle">very creative, professional, easy going and organized.</h3>
         </div>
-        <div>
+        <div className="section1__right">
           <img src="../images/lauraportillo" alt="laura" />
         </div>
       </section>
