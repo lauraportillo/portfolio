3b2d1c33d79a49a9dd1611fbfcb9c389c7d809ee
Laura Portillo <lauraportillo.ark@gmail.com>
2021-07-10 16:25:00 +0200

    add spanish description

--- src/components/AboutEn.js
+++ src/components/AboutEn.js
@@ -48,19 +48,6 @@ const AboutEn = (props) => {
           <li className="section2__lang--item">|</li>
           <li className="section2__lang--es">Es</li>
         </ul>
-
-        {/* <p className="section2__text--para">
-          Apasionada por el orden y la organización estudié Arquitectura, para poder construir ideas, estructurar
-          espacios, para encajar piezas, moverlas y ajustarlas. Admirada por la tecnología me embarco en este viaje como
-          desarrolladora front end, donde de nuevo tengo que construir, estructurar, encajar y mover código,
-          comunicándome con las máquinas. Mi experiencia trabajando con equipos multidisciplinares e internacionales me
-          han aportado una mentalidad abierta para relacionarme con otras culturas y métodos de trabajo. Soy una persona
-          creativa, trabajadora, me gusta tener todo bajo control, pero si no lo consigo, busco soluciones y pido ayuda
-          si es necesario. Mi mayor reto profesional ha sido dar este paso a la reinvención, y formar parte del bootcamp
-          de Adalab, que me ha dado las herramientas para continuar el camino. Una vez aquí, vengo dispuesta a trasladar
-          todas mis habilidades al mundo de la tecnología. Próximos objetivos: convertirme en Arquitecto al cuadrado,
-          esta vez en el campo de la tecnología!
-        </p> */}
       </section>
     </article>
   );
