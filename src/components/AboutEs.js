e1e3d6af614e1ad7e5a80ca95068249da2505d40
Laura Portillo <lauraportillo.ark@gmail.com>
2021-07-11 09:14:36 +0200

    add spanish title

--- src/components/AboutEs.js
+++ src/components/AboutEs.js
@@ -7,8 +7,10 @@ const AboutEs = (props) => {
     <article className="about">
       <section className="section1">
         <div className="section1__left">
-          <h2 className="section1__left--title">Front-end Developer & Architect</h2>
-          <h3 className="section1__left--subtitle">very creative, professional, easy going and organized.</h3>
+          <h2 className="section1__left--title">Programadora Front-end & Arquitecto</h2>
+          <h3 className="section1__left--subtitle">
+            creativa, muy organizada, profesional y buena trabajando en equipo.
+          </h3>
         </div>
         <div className="section1__right">
           <img className="section1__right--photo" src={image} alt="laura" />
