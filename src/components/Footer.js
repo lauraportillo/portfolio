d708acde25a149ae6eb00e57a6ab61d04efd39cd
Laura Portillo <lauraportillo.ark@gmail.com>
2021-07-11 09:40:14 +0200

    add info footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,14 @@ import React from 'react';
 import '../stylesheets/Footer.scss';
 
 const Footer = () => {
-  return <footer className="containerFooter"></footer>;
+  return (
+    <footer className="containerFooter">
+      <div className="containerFooter__copy">
+        <small>By Laura Portillo Rodríguez</small>
+        <small>©2021</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
